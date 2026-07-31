4a060de6234c7bd2487126bf6c856ddc38093fe4
tolaanfur-lgtm <tolaanfur@gmail.com>
2026-07-31 23:42:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -42,7 +42,7 @@ self.addEventListener("notificationclick", function (event) {
     })
   );
 });
-const CACHE = 'tolaanfur-v26';
+const CACHE = 'tolaanfur-v28';
 const FILES = [
   '/',
   '/index.html',
